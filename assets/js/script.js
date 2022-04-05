c44c0414cd0939a4efc09746a0439340340d7bc9
dogcraft <91873696+dogcraftmc@users.noreply.github.com>
2022-04-05 12:10:06 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const displayMenu = () => {
+  document.getElementById('dropdownNav').classList.toggle('show');
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown') && !event.target.matches('.dropdown__title')) {
+    const dropdownNav = document.getElementById('dropdownNav');
+    if (dropdownNav.classList.contains('show')) {
+      dropdownNav.classList.remove('show');
+    }
+  }
+}
+
+const detectMobile = () => {
+  const regex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return navigator.userAgent.match(regex) ? true : false ;
+};
+
+const isMobile = detectMobile();
+
+const appHeight = () => {
+  const vh = window.innerHeight/100;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+if (isMobile) {
+  appHeight();
+  window.addEventListener('resize', appHeight);
+}
